b8629a09a480c23fb81993ef7bc281653ef00674
hhhxiao <1417599491@qq.com>
2025-05-05 16:37:57 +0800

    Fix publish script error.

--- SMT.WPF/publish.py
+++ SMT.WPF/publish.py
@@ -2,6 +2,9 @@ import os, zipfile
 import shutil
 
 publish_path = "./bin/Release/net7.0-windows/publish"
+extra_folders = ["db", "glossaries"] #do not add / to the end
+extra_files = ["../changelog.md", "oo2core_6_win64.dll"]
+
 
 
 def zip_folder(folder_path, output_path, compression=zipfile.ZIP_DEFLATED):
@@ -16,7 +19,20 @@ def zip_folder(folder_path, output_path, compression=zipfile.ZIP_DEFLATED):
 code = 0
 if os.system("dotnet publish") != 0:
     exit(-1)
-copy_files = ["db/", "glossaries/", "../changelog.md", "oo2core_6_win64.dll"]
-for file in copy_files:
-    os.system("cp -r {} {}".format(file, publish_path))
+
+
+
+for file in extra_files:
+    base_name = os.path.basename(file)
+    print("copy file: " ,file)
+    shutil.copyfile(file, os.path.join(publish_path, base_name))
+
+for folder in extra_folders:
+    base_name = os.path.basename(folder)
+    full_path = os.path.join(publish_path, base_name)
+    if os.path.exists(full_path):
+        shutil.rmtree(full_path)
+    print("copy folder: " ,folder)
+    shutil.copytree(folder, full_path)
 zip_folder(publish_path, "../SoulsModTranslator.zip")
+print("Create archive finished.")
